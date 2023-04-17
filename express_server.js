bb06ddc4ad0d2ae28bfdebca53dc62dd3451b0a4
Rukiye Ozmen <rukiiye42@gmail.com>
2023-04-17 22:02:19 +0000

    added /hello with html

--- express_server.js
+++ express_server.js
@@ -15,6 +15,10 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get('/hello', (req, res) => {
+  res.send("<html><body>Hello !!! <b>World </body></html>");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
