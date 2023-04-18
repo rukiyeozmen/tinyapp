33fd53724c3e89ff84c16340917db9f1641aa36c
Rukiye Ozmen <rukiiye42@gmail.com>
2023-04-18 00:01:48 +0000

    added the sort and long url

--- express_server.js
+++ express_server.js
@@ -29,6 +29,14 @@ app.get('/hello', (req, res) => {
   res.send("<html><body>Hello !!! <b>World </body></html>");
 });
 
+app.get('/urls/:id', (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
+  res.render('urls_show', templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
