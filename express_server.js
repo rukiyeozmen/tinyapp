087140f822b903bc8c88086e75b73e03ea721921
Rukiye Ozmen <rukiiye42@gmail.com>
2023-04-19 12:00:40 +0000

    added delete button

--- express_server.js
+++ express_server.js
@@ -19,6 +19,11 @@ app.post("/urls", (req, res) => {
   console.log(urlDatabase);
   res.redirect(`/urls/${shortURL}`);
 });
+app.post('/urls/:id/delete', (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect('/urls');
+});
+
 app.get('/u/:id', (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
